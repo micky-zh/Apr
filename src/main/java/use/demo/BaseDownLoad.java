58f1706520c666968cc055160aab094e7becec6b
zhengfan <zhengfan@baidu.com>
2018-06-06 17:36:29 +0800

    update jiandan

--- src/main/java/use/demo/BaseDownLoad.java
+++ src/main/java/use/demo/BaseDownLoad.java
@@ -1,58 +1,24 @@
 package use.demo;
 
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.HttpException;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpVersion;
-import org.apache.http.NoHttpResponseException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.HttpRequestRetryHandler;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.params.CookiePolicy;
-import org.apache.http.client.params.HttpClientParams;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.conn.ConnectTimeoutException;
-import org.apache.http.conn.routing.HttpRoute;
-import org.apache.http.conn.routing.HttpRoutePlanner;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.params.SyncBasicHttpParams;
-import org.apache.http.protocol.HttpContext;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
-import javax.net.ssl.SSLException;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.X509Certificate;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by zhengfan on 2016/11/29 0029.
  */
 public abstract class BaseDownLoad {
 
-    final int timeout = 6000;
+    final int timeout = 30000;
 
     protected final Map<String, String> header = new HashMap<>();
 
@@ -90,112 +56,5 @@ public abstract class BaseDownLoad {
         return null;
     }
 
-    public HttpClient getHttpClient() {
-        HttpParams params = new SyncBasicHttpParams();
-        params.setParameter(HttpProtocolParams.PROTOCOL_VERSION,
-                HttpVersion.HTTP_1_1);
-        params.setBooleanParameter(HttpProtocolParams.USE_EXPECT_CONTINUE,
-                true);
-        params.setBooleanParameter(HttpConnectionParams.STALE_CONNECTION_CHECK,
-                true);
-        params.setIntParameter(HttpConnectionParams.SOCKET_BUFFER_SIZE,
-                8 * 1024);
-        params.setIntParameter(HttpConnectionParams.SO_TIMEOUT, 20000);
-        params.setIntParameter(HttpConnectionParams.CONNECTION_TIMEOUT, 5000);
-        params.setLongParameter(ClientPNames.CONN_MANAGER_TIMEOUT, 1000l); //该值就是连接不够用的时候等待超时时间，一定要设置，而且不能太大
-        params.setBooleanParameter(HttpConnectionParams.TCP_NODELAY, true);
-        params.setBooleanParameter(HttpConnectionParams.SO_REUSEADDR, true);
-
-        PoolingClientConnectionManager mgr = new PoolingClientConnectionManager();
-        mgr.setMaxTotal(2000);
-        mgr.setDefaultMaxPerRoute(500);
-
-        DefaultHttpClient httpclient = new DefaultHttpClient(mgr, params);
-        httpclient.setHttpRequestRetryHandler(new DefaultHttpRequestRetryHandler(0, false));
-
-        HttpClientParams.setCookiePolicy(httpclient.getParams(), CookiePolicy.BROWSER_COMPATIBILITY);
-        HttpProtocolParams.setUserAgent(httpclient.getParams(),
-                "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.18) Gecko/20110628 Ubuntu/10.04 (lucid) Firefox/3"
-                        + ".6.18");
-
-        //创建SSLSocketFactory
-        try {
-            SSLSocketFactory socketFactory = new SSLSocketFactory(new TrustStrategy() {
-                public boolean isTrusted(X509Certificate[] chain, String authType)
-                        throws java.security.cert.CertificateException {
-                    return true;
-                }
-            }, SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-            httpclient.getConnectionManager().getSchemeRegistry().register(new Scheme("https", 443, socketFactory));
-        } catch (KeyManagementException e) {
-            throw new SecurityException(e.getMessage(), e);
-        } catch (KeyStoreException e) {
-            throw new SecurityException(e.getMessage(), e);
-        } catch (UnrecoverableKeyException e) {
-            throw new SecurityException(e.getMessage(), e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new SecurityException(e.getMessage(), e);
-        }
-
-        return httpclient;
-    }
-
-    public class DynamicProxyRoutePlanner implements HttpRoutePlanner {
-
-        private DefaultProxyRoutePlanner defaultProxyRoutePlanner = null;
-
-        public DynamicProxyRoutePlanner(HttpHost host){
-            defaultProxyRoutePlanner = new DefaultProxyRoutePlanner(host);
-        }
-
-        public void setProxy(HttpHost host){
-            defaultProxyRoutePlanner = new DefaultProxyRoutePlanner(host);
-        }
-
-        public HttpRoute determineRoute(HttpHost target, HttpRequest request, HttpContext context)
-                throws HttpException {
-            return defaultProxyRoutePlanner.determineRoute(target,request,context);
-        }
-    }
-
-    static HttpHost proxy = new HttpHost("127.0.0.1", 80);
-    DynamicProxyRoutePlanner routePlanner = new DynamicProxyRoutePlanner(proxy);
-
-    private CloseableHttpClient getInstanceClient() {
-        CloseableHttpClient httpClient;
-        //StandardHttpRequestRetryHandler standardHandler = new StandardHttpRequestRetryHandler(2, true);
-        HttpRequestRetryHandler handler = new HttpRequestRetryHandler() {
-
-            public boolean retryRequest(IOException arg0, int retryTimes, HttpContext arg2) {
-                if (arg0 instanceof UnknownHostException || arg0 instanceof ConnectTimeoutException
-                        || !(arg0 instanceof SSLException) || arg0 instanceof NoHttpResponseException) {
-                    return true;
-                }
-                if (retryTimes > 1) {
-                    return false;
-                }
-                HttpClientContext clientContext = HttpClientContext.adapt(arg2);
-                HttpRequest request = clientContext.getRequest();
-                boolean idempotent = !(request instanceof HttpEntityEnclosingRequest);
-                if (idempotent) {
-                    // 如果请求被认为是幂等的，那么就重试。即重复执行不影响程序其他效果的
-                    return true;
-                }
-                return false;
-            }
-        };
-        RequestConfig defaultRequestConfig = RequestConfig.custom()
-                .setConnectTimeout(3000)
-                .setSocketTimeout(1000)
-                .setConnectionRequestTimeout(3000)
-                .build();
-        // 设置代理ip
-
-        httpClient = HttpClients.custom().setRoutePlanner(routePlanner).setRetryHandler(handler)
-                .setConnectionTimeToLive(1, TimeUnit.MINUTES).setDefaultRequestConfig(defaultRequestConfig)
-                .build();
-
-        return httpClient;
-    }
 
 }
--- src/main/java/use/demo/cases/JianDan.java
+++ src/main/java/use/demo/cases/JianDan.java
@@ -1,19 +1,31 @@
 package use.demo.cases;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import use.demo.BaseDownLoad;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import use.demo.BaseDownLoad;
 
 /**
  * Created by zhengfan on 2016/11/25 0025.
@@ -23,11 +35,32 @@ public class JianDan extends BaseDownLoad {
 
     private static Logger LOGGER = LoggerFactory.getLogger(JianDan.class);
 
+    private String INDEX_URL = "https://jandan.net/ooxx";
+
     private String baseDir = "c:\\SinaImage\\";
 
     private int pages = 10;
 
     private int index = 1;
+
+    private OkHttpClient client;
+
+    private ScriptEngineManager factory = new ScriptEngineManager();
+
+    private ScriptEngine engine = factory.getEngineByName("JavaScript");
+
+    public JianDan() throws Exception {
+        // read script file
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        engine.eval(Files.newBufferedReader(Paths.get(s + "/scripts/jiandan.js"), StandardCharsets.UTF_8));
+
+        client = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+    }
 
     public void setBaseDir(String baseDir) {
         this.baseDir = baseDir;
@@ -36,13 +69,6 @@ public class JianDan extends BaseDownLoad {
     public void setPages(int pages) {
         this.pages = pages;
     }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-
-    private String INDEX_URL = "https://jandan.net/ooxx";
 
     private void init() {
         try {
@@ -53,17 +79,7 @@ public class JianDan extends BaseDownLoad {
         }
     }
 
-
-//    @Test
-//    public void getPath() throws MalformedURLException {
-//        URL url = new URL("http://ww1.sinaimg.cn/mw690/707c5757gw1f9shjpofvrg20b4068b29.gif");
-//
-//        System.out.println(FilenameUtils.getBaseName(url.getPath())); // -> file
-//        System.out.println(FilenameUtils.getExtension(url.getPath())); // -> xml
-//        System.out.println(FilenameUtils.getName(url.getPath())); // -> file.xml
-//    }
-
-    public void downLoadImage(String url) throws IOException {
+    private void downLoadImage(String url) throws IOException {
         final String fileName = FilenameUtils.getName(new URL(url).getPath());
         File tmpFile = new File(baseDir + fileName);
 
@@ -74,8 +90,11 @@ public class JianDan extends BaseDownLoad {
         LOGGER.info("downLoad url: {}", url);
         for (int i = 1; i <= 3; i++) {
             try {
-                Connection.Response rep = initConnection(url).execute();
-                FileUtils.writeByteArrayToFile(new File(baseDir + fileName), rep.bodyAsBytes());
+                // Connection.Response rep = initConnection(url).execute();
+                Request request = new Request.Builder().url(url).build();
+                Response response = client.newCall(request).execute();
+                ResponseBody rep = response.body();
+                FileUtils.writeByteArrayToFile(new File(baseDir + fileName), rep.bytes());
                 break; // Break immediately if successful
             } catch (IOException e) {
                 System.out.println(e.getMessage());
@@ -85,64 +104,38 @@ public class JianDan extends BaseDownLoad {
 
     }
 
-    private void parseCurrent() throws IOException {
-        Connection.Response r = initConnectionWithHeader(INDEX_URL).execute();
-        Document doc = r.parse();
-        Element element = doc.select("span.current-comment-page").first();
-        if (element == null) {
-            LOGGER.warn("not found page number info.");
-            return;
-        }
+    private String runJS(String code) throws ScriptException, IOException, NoSuchMethodException {
 
-        Matcher m = Pattern.compile("\\d+").matcher(element.text());
-        int last = Integer.MAX_VALUE;
-        if (m.find()) {
-            index = Integer.valueOf(m.group());
-            last = index - pages;
-            LOGGER.info("current page is {}", index);
-        }
-
-        final String pageUrl = INDEX_URL + "/page-";
-        for (int i = index; i > last; i--) {
-            try {
-                parseContent(pageUrl + i);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        Invocable inv = (Invocable) engine;
+        // call function from script file
+        Object ss =
+                inv.invokeFunction("jdymYkosnJZRKejkAMubgc9PgyjpU8tD31", code, "tcrbe7l5qVTQjh5Hr9F2oC9TUPutBpb5");
+        return ("http:" + ss).replaceAll("cn/.*?/", "cn/large/").replaceAll("//ww.","//wx4");
     }
 
-
-    private void parseContent(String url) throws IOException {
-        LOGGER.debug("current request url is {}", url);
+    private void parse(String url, int counter) throws IOException, ScriptException, NoSuchMethodException {
+        LOGGER.info("current request url is {}, page : {}", url, counter);
         Document doc = initConnection(url).get();
-        for (Element element : doc.select("a.view_img_link")) {
-            //System.out.println(element.attr("href"));
-            String href = element.attr("href");
-            downLoadImage(href.startsWith("//") ? "http:" + href : href);
+        for (Element element : doc.select("span.img-hash")) {
+            String image = runJS(element.text());
+            downLoadImage(image);
         }
+        if (counter <= this.pages) {
+            Element e = doc.select(".previous-comment-page").first();
+            String nextPage = "http:" + e.attr("href");
+            parse(nextPage, counter + 1);
+        }
+
     }
 
-    public static void main(String[] args) throws IOException {
-        if (args.length > 1 && ("help".equals(args[0].toLowerCase()) || "-help".equals(args[0].toLowerCase()))) {
-            System.out.println("USE: java -jar xxx.jar  dir start end");
-            System.out.println("USE: java -jar xxx.jar 1 100 0 y");
-            System.exit(0);
-        }
+    private void start() throws Exception {
+        parse(INDEX_URL, 0);
+    }
 
-        JianDan jiandan = new JianDan();
-        if (args.length >= 1) {
-            jiandan.setBaseDir(args[0]);
-        }
-
-        if (args.length >= 2) {
-            jiandan.setIndex(Integer.valueOf(args[1]));
-        }
-
-        if (args.length >= 3) {
-            jiandan.setPages(Integer.valueOf(args[2]));
-        }
-        jiandan.init();
-        jiandan.parseCurrent();
+    public static void main(String[] args) throws Exception {
+        JianDan j = new JianDan();
+        j.init();
+        j.pages = 1000;
+        j.start();
     }
 }
